505b4d59da2d01bbbfa65aaa751bcbfdff31b57b
Pete Woods <pete.woods@canonical.com>
2014-05-23 12:14:04 +0100

    Add nonsense test just to show that the Vimeo API works

--- tests/unit/test-vimeo-scope.cpp
+++ tests/unit/test-vimeo-scope.cpp
@@ -16,20 +16,45 @@
  * Author: Pete Woods <pete.woods@canonical.com>
  */
 
+#include <core/net/error.h>
+#include <core/net/http/client.h>
+#include <core/net/http/content_type.h>
+#include <core/net/http/request.h>
+#include <core/net/http/response.h>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <json/json.h>
+
+namespace http = core::net::http;
+namespace json = Json;
+namespace net = core::net;
 
 using namespace std;
 using namespace testing;
 
 namespace {
 
+auto default_progress_reporter =
+        [](const http::Request::Progress& progress)
+        {
+            return http::Request::Progress::Next::continue_operation;
+        };
+
 class TestVimeoScope: public Test {
 protected:
-
 };
 
 TEST_F(TestVimeoScope, Foo) {
+    auto client = http::make_client();
+    string url = "http://vimeo.com/api/v2/channel/delicioussandwich/info.json";
+    auto request = client->head(
+            http::Request::Configuration::from_uri_as_string(url));
+    auto response = request->execute(default_progress_reporter);
+
+    json::Value root;
+    json::Reader reader;
+    reader.parse(response.body, root);
+    EXPECT_EQ("A Sandwich Blog", root["description"].asString());
 }
 
 } // namespace
